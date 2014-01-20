22a73b70c20565c9bdf559da073b3afe18b3f1ac
Rafał Studnicki <rafal@opencubeware.org>
2014-01-20 23:01:14 +0100

    Add damage report ops

--- src/basho_bench_driver_insurance.erl
+++ src/basho_bench_driver_insurance.erl
@@ -6,8 +6,9 @@
 
 -record(state, {username, password,
                 users,
-                role, roles,
-                param, params,
+                role, roles = [],
+                param, params = [],
+                damage, damages = [],
                 decision, tickets = [], attachments = []}).
 %% ====================================================================
 %% API
@@ -27,6 +28,8 @@ run({role, Op}, KeyGen, ValueGen, State) ->
     check_return(run_role(Op, KeyGen, ValueGen, State));
 run({param, Op}, KeyGen, ValueGen, State) ->
     check_return(run_param(Op, KeyGen, ValueGen, State));
+run({damage, Op}, KeyGen, ValueGen, State) ->
+    check_return(run_damage(Op, KeyGen, ValueGen, State));
 run({decision, Op}, KeyGen, ValueGen, State) ->
     check_return(run_decision(Op, KeyGen, ValueGen, State));
 run(Op, _KeyGen, _ValueGen, State) ->
@@ -94,6 +97,34 @@ run_param(setUserParam, _, _, #state{username=Username, param=Params}=State) ->
                             [Username, Key, Value]),
     {Return, State#state{params=[Key|Params]}};
 run_param(_Op, _, _, State) ->
+    {silent, State}.
+
+run_damage(reportInformation, _, _, #state{damages=Damages}=State) ->
+    case detergent:call(service(damages, State), "ReportInformation",
+                        [random_string(15) || _ <- lists:seq(1,3)]) of
+        {ok, _, [{_, _, Id}]}=Reply ->
+            {Reply, State#state{damages=[Id|Damages]}};
+        Other ->
+            {Other, State}
+    end;
+run_damage(damageInformation, _, _, #state{damages=[Id|_]}=State) ->
+    R = random_string(15),
+    Return = detergent:call(service(damages, State), "DamageInformation",
+                            [Id, "2013-01-20", R, R, R, true]),
+    {Return, State};
+run_damage(personReportingDamageData, _, _, #state{damages=[Id|_]}=State) ->
+    R = random_string(15),
+    Return = detergent:call(service(damages, State), "PersonReportingDamageData",
+                            [Id,R,R,"2312","mail@mail.com",R,"13212",R,R,R,R]),
+    {Return, State};
+run_damage(ownerDamagedVehicleData, _, _, #state{damages=[Id|_]}=State) ->
+    Return = detergent:call(service(damages, State), "OwnerDamagedVehicleData",
+                            [Id, 2, "01234567890"]),
+    {Return, State};
+run_damage(confirmation, _, _, #state{damages=[Id|_]}=State) ->
+    Return = detergent:call(service(damages, State), "Confirmation", [Id]),
+    {Return, State};
+run_damage(_Op, _, _, State) ->
     {silent, State}.
 
 run_decision(addTicket, _, _, #state{tickets=Data}=State) ->
@@ -171,7 +202,8 @@ password(Id) -> "password" ++ integer_to_list(Id).
 service(users, State) -> State#state.users;
 service(roles, State) -> State#state.role;
 service(params, State) -> State#state.param;
-service(decision, State) -> State#state.decision.
+service(decision, State) -> State#state.decision;
+service(damages, State) -> State#state.damage. 
 
 prepare_wsdls() ->
     UsersEndpoint = basho_bench_config:get(users_endpoint),
@@ -180,11 +212,14 @@ prepare_wsdls() ->
     RolesWsdl = detergent:initModel(RolesEndpoint),
     ParamsEndpoint = basho_bench_config:get(params_endpoint),
     ParamsWsdl = detergent:initModel(ParamsEndpoint),
+    DamageEndpoint = basho_bench_config:get(damage_endpoint),
+    DamageWsdl = detergent:initModel(DamageEndpoint),
     DecisionEndpoint = basho_bench_config:get(decision_endpoint),
     DecisionWsdl = scrub:initModel(DecisionEndpoint),
     #state{users = UsersWsdl,
            role = RolesWsdl,
            param = ParamsWsdl,
+           damage = DamageWsdl,
            decision=DecisionWsdl}.
 
 iso_8601_fmt(DateTime) ->
